543d22060db24167923226eb3f3092739b9edd6f
cnxk <cos4338@gmail.com>
2022-11-24 23:39:04 +0000

    Feat: Contant 데이터 수집 폴더 생성

---
+++ src/constant/Constant.js
@@ -0,0 +1,13 @@
+export const Place_Holder = {
+  email: '이메일을 입력해주세요.',
+  password: '비밀번호를 입력해주세요.',
+  password_confirm: '비밀번호를 다시 한 번 입력해주세요',
+  name: '이름을 입력해주세요.',
+};
+
+export const LabelText = {
+  email: '이메일',
+  password: '비밀번호',
+  password_confirm: '비밀번호 확인',
+  name: '이름',
+};
