26d61afb57649c15b43f1af09ef914613825bea4
cnxk <cos4338@gmail.com>
2022-11-25 20:41:02 +0000

    Feat: Create useFetch customHook to handle get data

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,5 +1,10 @@
 import { useQuery } from 'react-query';
+import Http from '../services/Http';
 
-const useFetch = () => {
-  const { isLoading } = useQuery;
+const useFetch = (key, id, url) => {
+  const fetchItem = new Http(url);
+  const { data, isError } = useQuery([key, id], () => fetchItem.get());
+  return { data, isError };
 };
+
+export default useFetch;
